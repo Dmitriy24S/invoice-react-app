47c798d35ec3b8265ea6ad7e5646e966984b67ae
Dmitriy24S <devilins24@gmail.com>
2022-06-28 20:28:00 +0300

    feat: :sparkles: add dropdown with filtering options for invoices, filtering func

--- src/components/InvoiceList.tsx
+++ src/components/InvoiceList.tsx
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import data from "../data/data.json";
+import { ReactComponent as ArrowDown } from "../images/icon-arrow-down.svg";
 import { ReactComponent as PlusIcon } from "../images/icon-plus.svg";
 
 const PaidStatus = ({ status }: any) => {
@@ -39,6 +40,62 @@ const DraftStatus = ({ status }: any) => {
 };
 
 const InvoiceList = () => {
+  const [invoices, setInvoices] = useState(data);
+  const [filteredInvoices, setFilteredInvoices] = useState<any>(null);
+  const [selectedFilter, setSelectedFilter] = useState<any>(null);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdown = useRef<HTMLDivElement>(null);
+
+  // close dropdown on click outside dropdown
+  function handleClickOutside(e: any) {
+    if (dropdown.current && !dropdown.current.contains(e.target)) {
+      setIsDropdownOpen(false);
+    }
+  }
+  // close dropdown on click outside dropdown
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isDropdownOpen]);
+
+  const [invoiceListFilterOptions, setInvoiceListFilterOptions] = useState([
+    { id: 0, name: "all", checked: true },
+    { id: 1, name: "paid", checked: false },
+    { id: 2, name: "pending", checked: false },
+    { id: 3, name: "draft", checked: false },
+  ]);
+
+  // Handle filter option click/select, update filter options array checked status, set selected filter option
+  const handleSelect = (filterName: string) => {
+    const updatedListFilterOptions = invoiceListFilterOptions.map(
+      (filterOption) => {
+        if (filterOption.name === filterName) {
+          setSelectedFilter(filterOption.checked ? null : filterOption.name); // if it's new filter option set it as new selected filter state value, otherwise null
+          return { ...filterOption, checked: !filterOption.checked }; // update filter options array checked status
+        } else {
+          return { ...filterOption, checked: false }; // false checked status
+        }
+      }
+    );
+    setInvoiceListFilterOptions(updatedListFilterOptions);
+  };
+
+  // Update and filter the invoices if there is new selected filter option
+  useEffect(() => {
+    setFilteredInvoices(invoices); // initial set to all invoices
+    if (invoices && selectedFilter) {
+      if (selectedFilter === "all") {
+        setFilteredInvoices(invoices); // if select all then return and show all invoices
+      } else {
+        setFilteredInvoices(
+          invoices.filter((invoice) => invoice.status === selectedFilter) // filter invoices that match selected filter type
+        );
+      }
+    }
+  }, [selectedFilter, invoices]);
+
   return (
     <section className="mt-8 px-4">
       <div className="list-header">
@@ -48,8 +105,40 @@ const InvoiceList = () => {
             <p className="opacity-80 mt-1">{data.length} invoices</p>
           </div>
           <div className="list-header-right flex items-center gap-5">
-            <button className="font-bold">Filter</button>
-            <button className="new-invoice-btn flex gap-2 sitems-center bg-violet-500 px-3 py-2.5 rounded-full font-bold">
+            <div className="dropdown-container relative" ref={dropdown}>
+              <button
+                className="font-bold flex gap-2 items-center"
+                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              >
+                Filter{" "}
+                <ArrowDown
+                  className={`transition-transform
+                  ${isDropdownOpen && "-rotate-180"}`}
+                />
+              </button>
+              {/* Dropdown */}
+              {isDropdownOpen && (
+                <div className="dropdown absolute top-12 min-w-[10rem] bg-indigo-900 bg-opacity-40 flex flex-col gap-2 p-4 left-1/2 -translate-x-2/4 rounded-lg">
+                  {invoiceListFilterOptions.map((filterOption) => (
+                    <label
+                      htmlFor={filterOption.name}
+                      className="flex gap-2 items-center cursor-pointer capitalize"
+                      key={filterOption.id}
+                    >
+                      <input
+                        type="radio"
+                        name="radio"
+                        id={filterOption.name}
+                        checked={filterOption.checked}
+                        onChange={() => handleSelect(filterOption.name)}
+                      />
+                      {filterOption.name}
+                    </label>
+                  ))}
+                </div>
+              )}
+            </div>
+            <button className="new-invoice-btn flex gap-2 sitems-center bg-violet-500 px-4 py-2.5 rounded-full font-bold">
               <div className="plus-icon-container bg-white rounded-full w-6 h-6 flex items-center justify-center">
                 <PlusIcon />
               </div>
@@ -58,7 +147,7 @@ const InvoiceList = () => {
           </div>
         </div>
         <div className="list flex flex-col gap-5 justify-center items-center mt-8">
-          {data.map((item) => (
+          {filteredInvoices?.map((item: any) => (
             <div
               className="item min-w-[18rem] w-full max-w-[20rem] bg-indigo-900 bg-opacity-40 p-4 px-5 rounded-lg"
               key={item.id}
