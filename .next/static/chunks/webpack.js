318b91644fdd6bbe7420c06478851dcaf3e586c6
Dmitriy24S <devilins24@gmail.com>
2022-06-29 15:23:31 +0300

    nextjs migration, gh-pages test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "29944a45aa7c8444"; }
+/******/ 		__webpack_require__.h = function() { return "9630965594228a69"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
