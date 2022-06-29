ea420399ba3e4fabb0f84f29dc82b5c6d80689b3
Dmitriy24S <devilins24@gmail.com>
2022-06-29 15:01:50 +0300

    build: :wrench: migrate to nextjs from cra, for invoice page route

---
+++ components/Header.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import React from "react";
+// import { ReactComponent as DarkThemeIcon } from "../images/icon-moon.svg";
+// import { ReactComponent as LightThemeIcon } from "../images/icon-sun.svg";
+
+const Header = ({ darkTheme, setDarkTheme }: any) => {
+  const toggleDarkTheme = () => {
+    setDarkTheme((prevTheme: boolean) => !prevTheme);
+  };
+  return (
+    // bg-[#1e2139] - header bg?
+    <header className="bg-indigo-900 bg-opacity-40">
+      <div className="header-content max-w-5xl mx-auto flex justify-between">
+        <div className="logo-container w-16 h-16 rounded-r-2xl overflow-hidden bg-violet-500  flex justify-center items-center relative after:h-3/6 after:bg-white after:opacity-20 after:w-full after:absolute after:bottom-0 after:rounded-tl-2xl">
+          {/* <Logo className="z-10" /> */}
+          <Image src="/images/logo.svg" alt="Logo" width={28} height={26} />
+        </div>
+        <div className="flex items-center">
+          <button
+            aria-label="toggle dark theme"
+            onClick={toggleDarkTheme}
+            className="pr-6 pl-4 h-full flex items-center"
+          >
+            {/* {darkTheme ? <DarkThemeIcon /> : <LightThemeIcon />} */}
+            {darkTheme ? (
+              <Image
+                src="/images/icon-moon.svg"
+                alt="Dark theme icon"
+                width={20}
+                height={20}
+              />
+            ) : (
+              <Image
+                src="/images/icon-sun.svg"
+                alt="Light theme icon"
+                width={20}
+                height={20}
+              />
+            )}
+          </button>
+          {/* #494e6e - border bg? */}
+          <button
+            className="avatar-container pl-4 border-l border-[#716da8] h-full flex items-center justify-center pr-6 cursor-pointer"
+            aria-label="open profile"
+          >
+            <Image
+              src="/images/image-avatar.jpg"
+              alt="avatar"
+              width={32}
+              height={32}
+            />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/InvoiceList.tsx
@@ -0,0 +1,205 @@
+import dayjs from "dayjs";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useRef, useState } from "react";
+import data from "../data/data.json";
+
+const PaidStatus = ({ status }: any) => {
+  // html circle/dot: &#x2022;
+  return (
+    <div className="bg-[#33d69f] bg-opacity-10 rounded-lg w-[7rem] capitalize text-center">
+      <p className="px-2 py-2.5 text-[#33d69f] font-bold">
+        <span className="circle h-2.5 w-2.5 bg-[#33d69f] inline-block rounded-full mr-2"></span>
+        {status}
+      </p>
+    </div>
+  );
+};
+const PendingStatus = ({ status }: any) => {
+  return (
+    <div className="bg-[#ff8f00] bg-opacity-10 rounded-lg w-[7rem] capitalize text-center">
+      <p className="px-2 py-2.5 text-[#ff8f00] font-bold">
+        <span className="circle h-2.5 w-2.5 bg-[#ff8f00] inline-block rounded-full mr-2"></span>
+        {status}
+      </p>
+    </div>
+  );
+};
+
+const DraftStatus = ({ status }: any) => {
+  return (
+    // light theme #373b53
+    // dark theme #dfe3fa
+    <div className="bg-[#dfe3fa] bg-opacity-10 rounded-lg w-[7rem] capitalize text-center">
+      <p className="px-2 py-2.5 text-[#dfe3fa] font-bold">
+        <span className="circle h-2.5 w-2.5 bg-[#dfe3fa] inline-block rounded-full mr-2"></span>
+        {status}
+      </p>
+    </div>
+  );
+};
+
+const InvoiceList = () => {
+  const [invoices, setInvoices] = useState(data);
+  const [filteredInvoices, setFilteredInvoices] = useState<any>(null);
+  const [selectedFilter, setSelectedFilter] = useState<any>(null);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdown = useRef<HTMLDivElement>(null);
+
+  // close dropdown on click outside dropdown
+  function handleClickOutside(e: any) {
+    if (dropdown.current && !dropdown.current.contains(e.target)) {
+      setIsDropdownOpen(false);
+    }
+  }
+  // close dropdown on click outside dropdown
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isDropdownOpen]);
+
+  const [invoiceListFilterOptions, setInvoiceListFilterOptions] = useState([
+    { id: 0, name: "all", checked: true },
+    { id: 1, name: "paid", checked: false },
+    { id: 2, name: "pending", checked: false },
+    { id: 3, name: "draft", checked: false },
+  ]);
+
+  // Handle filter option click/select, update filter options array checked status, set selected filter option
+  const handleSelect = (filterName: string) => {
+    const updatedListFilterOptions = invoiceListFilterOptions.map(
+      (filterOption) => {
+        if (filterOption.name === filterName) {
+          setSelectedFilter(filterOption.checked ? null : filterOption.name); // if it's new filter option set it as new selected filter state value, otherwise null
+          return { ...filterOption, checked: !filterOption.checked }; // update filter options array checked status
+        } else {
+          return { ...filterOption, checked: false }; // false checked status
+        }
+      }
+    );
+    setInvoiceListFilterOptions(updatedListFilterOptions);
+  };
+
+  // Update and filter the invoices if there is new selected filter option
+  useEffect(() => {
+    setFilteredInvoices(invoices); // initial set to all invoices
+    if (invoices && selectedFilter) {
+      if (selectedFilter === "all") {
+        setFilteredInvoices(invoices); // if select all then return and show all invoices
+      } else {
+        setFilteredInvoices(
+          invoices.filter((invoice) => invoice.status === selectedFilter) // filter invoices that match selected filter type
+        );
+      }
+    }
+  }, [selectedFilter, invoices]);
+
+  //   const handleClickInvoiceForDetails = () => {
+  //     return <InvoiceInfo />;
+  //   };
+
+  return (
+    <section className="mt-8 px-4">
+      <div className="list-header">
+        <div className="list-header-content max-w-5xl mx-auto flex justify-between">
+          <div className="list-header-left">
+            <h2 className="text-2xl font-bold">Invoices</h2>
+            <p className="opacity-80 mt-1">{data.length} invoices</p>
+          </div>
+          <div className="list-header-right flex items-center gap-5">
+            <div className="dropdown-container relative" ref={dropdown}>
+              <button
+                className="font-bold flex gap-2 items-center"
+                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              >
+                Filter{" "}
+                <Image
+                  src="/images/icon-arrow-down.svg"
+                  className={`transition-transform
+                  ${isDropdownOpen && "-rotate-180"}`}
+                  width="11"
+                  height="7"
+                />
+              </button>
+              {/* Dropdown */}
+              {isDropdownOpen && (
+                <div className="dropdown absolute top-12 min-w-[10rem] bg-indigo-900 bg-opacity-40 flex flex-col gap-2 p-4 left-1/2 -translate-x-2/4 rounded-lg">
+                  {invoiceListFilterOptions.map((filterOption) => (
+                    <label
+                      htmlFor={filterOption.name}
+                      className="flex gap-2 items-center cursor-pointer capitalize"
+                      key={filterOption.id}
+                    >
+                      <input
+                        type="radio"
+                        name="radio"
+                        id={filterOption.name}
+                        checked={filterOption.checked}
+                        onChange={() => handleSelect(filterOption.name)}
+                      />
+                      {filterOption.name}
+                    </label>
+                  ))}
+                </div>
+              )}
+            </div>
+            <button className="new-invoice-btn flex gap-2 sitems-center bg-violet-500 px-4 py-2.5 rounded-full font-bold">
+              <div className="plus-icon-container bg-white rounded-full w-6 h-6 flex items-center justify-center">
+                <Image src="/images/icon-plus.svg" width={11} height={11} />
+              </div>
+              New
+            </button>
+          </div>
+        </div>
+        <div className="list flex flex-col gap-5 justify-center items-center mt-8">
+          {filteredInvoices?.map((item: any) => (
+            // Invoice preview
+            <Link
+              href={`/invoice/${item.id}`}
+              passHref={true}
+              scroll={false}
+              //   className="item min-w-[18rem] w-full max-w-[20rem] bg-indigo-900 bg-opacity-40 p-4 px-5 rounded-lg"
+              key={item.id}
+            >
+              <article className="item min-w-[18rem] w-full max-w-[20rem] bg-indigo-900 bg-opacity-40 p-4 px-5 rounded-lg cursor-pointer">
+                <div className="item-title flex justify-between">
+                  <p className="font-bold">
+                    <span className="text-indigo-400">#</span>
+                    {item.id}
+                  </p>
+                  <p className="opacity-90">{item.clientName}</p>
+                </div>
+                <div className="item-info flex justify-between items-center mt-3">
+                  <div className="item-payment-total">
+                    <p className="opacity-90 text-sm">
+                      Due {dayjs(item.paymentDue).format("DD MMM YYYY")}
+                    </p>
+                    <p className="font-bold mt-1">
+                      ${" "}
+                      {item.total.toLocaleString("en-US", {
+                        minimumFractionDigits: 2,
+                      })}
+                    </p>
+                  </div>
+                  <div className="item-payment-status">
+                    {item.status === "paid" ? (
+                      <PaidStatus status={item.status} />
+                    ) : item.status === "pending" ? (
+                      <PendingStatus status={item.status} />
+                    ) : (
+                      <DraftStatus status={item.status} />
+                    )}
+                  </div>
+                </div>
+              </article>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default InvoiceList;
